bddbeedc6fdecbb0aa5538a6854dd074967326d0
Chris <mdhornet90@gmail.com>
2017-12-28 15:14:40 -0500

    Taking yet another step backwards, going to train a much more basic algorithm.

--- deeplearning.py
+++ deeplearning.py
@@ -1,21 +1,8 @@
-import h5py
-import definitions
-from os import path
-
-
-def load_train_data(location=path.join(definitions.DATASETS, 'train.h5')):
-    with h5py.File(location, mode='r') as train:
-        temp_in = train['train_set_x'][:]
-        temp_out = train['train_set_y'][:]
-        temp_classes = train['list_classes'][:]
-
-    return temp_in, temp_out, temp_classes
-
-
-def transform_matrices_to_vectors(matrices):
-    return matrices.reshape(matrices.shape[0], -1).T
+import numpy as np
 
 
 if __name__ == '__main__':
-    train_in, train_out, classification_options = load_train_data()
-    trans_in = transform_matrices_to_vectors(train_in)
+    train_input = np.asarray([[1, 2], [2, 3], [4, 6]])  # TODO function to create arbitrarily large training sets
+    train_output = np.asarray([3, 5, 10])
+    print(train_input)
+    print(train_output)
